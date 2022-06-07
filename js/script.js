8e41ee64624bc04dca7434ba64cc500db0410706
Fabian Aguirre <fabian@cosmico.cl>
2022-06-07 17:18:27 -0400

    primera carga entrega final 03

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $("#hamb-icon").click(function(){
+    $('#menuM').animate({width:'toggle'},300);
+    });
+    });
+
+    $(document).ready(function(){
+        $("#close").click(function(){
+        $('#menuM').animate({width:'toggle'},300);
+        });
+        });
+
+
+        //Get the button:
+mybutton = document.getElementById("botonTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
